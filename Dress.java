4d86f274dbdec3baf8c0e767a703b2a4d9c675dd
unknown <amith.s.xworkz@gmail.com>
2024-04-13 12:20:21 +0530

    removing deleted files from tracking

---
+++ Dress.java
@@ -0,0 +1,37 @@
+//local variables
+
+class Dress{
+	
+	public static void main(String []args){
+	
+     	int numberOfDressShop;
+		numberOfDressShop=26;
+		char colourOfDress;
+		colourOfDress='W';
+		byte numberOfDress;
+		numberOfDress=100;
+		short shopLicenceNumber;
+		shopLicenceNumber=5675;
+		long ownerPhoneNumber;
+		ownerPhoneNumber=8971098822l;
+		float costOfTheDress;
+		costOfTheDress=1000.9783f;
+		double gstOfTheShop;
+		gstOfTheShop=27698.86464576d;
+		boolean isShopAvailable;
+		isShopAvailable=true;
+		String shopName;
+		shopName="Vinayaka Stores";
+		
+		System.out.println("total number of dress shop is===="+numberOfDressShop);
+		System.out.println("colourOfDress is==="+colourOfDress);
+		System.out.println("number of dress is==="+numberOfDress);
+		System.out.println("shop licence num is==="+shopLicenceNumber);
+		System.out.println("owner phone number is==="+ownerPhoneNumber);
+		System.out.println("cost of the dress is==="+costOfTheDress);
+		System.out.println("gst of the shop is==="+gstOfTheShop);
+		System.out.println("is shop available==="+isShopAvailable);
+		System.out.println("name of the shop is==="+shopName);
+	}
+}
+		
---
+++ Laptop.java
@@ -0,0 +1,37 @@
+//Local variables
+
+class Laptop{
+	
+	public static void main(String []args){
+	
+	    int numberOfShowroom;
+		numberOfShowroom=20;
+		char seriesOfTheCompany;
+		seriesOfTheCompany='A';
+		byte numberOfLaptopAvailable;
+		numberOfLaptopAvailable=120;
+		short shopLicenceNumber;
+		shopLicenceNumber=5675;
+		long customerPhoneNumber;
+		customerPhoneNumber=8971098822l;
+		float costOfTheLaptop;
+		costOfTheLaptop=60000.9783f;
+		double gstOfTheShop;
+		gstOfTheShop=2769.86464576d;
+		boolean isShopAvailable;
+		isShopAvailable=true;
+		String laptopCompanyName;
+		laptopCompanyName="DELL";
+		
+		System.out.println("total number of showroom is===="+numberOfShowroom);
+		System.out.println("series of the company is==="+seriesOfTheCompany);
+		System.out.println("number of laptop available is==="+numberOfLaptopAvailable);
+		System.out.println("shop licence num is==="+shopLicenceNumber);
+		System.out.println("customer phone number is==="+customerPhoneNumber);
+		System.out.println("cost of the laptop is==="+costOfTheLaptop);
+		System.out.println("gst of the shop is==="+gstOfTheShop);
+		System.out.println("is shop available==="+isShopAvailable);
+		System.out.println("company name of the laptop is==="+laptopCompanyName);
+	}
+}
+		
