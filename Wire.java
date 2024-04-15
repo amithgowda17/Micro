cc7acdd87dc00df8bc63546f919b48b0dc85f615
unknown <amith.s.xworkz@gmail.com>
2024-04-15 08:53:47 +0530

    OR operator

---
+++ Wire.java
@@ -0,0 +1,30 @@
+class Wire{
+	
+	public static void main(String []args){
+		
+		int noOfWire=4;
+        String colourOfWire="Red";	
+		
+		if(noOfWire==4 || colourOfWire=="Red"){
+			
+			System.out.println("desired wire is selected");
+		}else{
+			System.out.println("wire not selected");
+		}
+		
+		if(noOfWire==4 || colourOfWire=="Yellow"){
+			
+			System.out.println("desired wire is selected");
+		}else{
+			System.out.println("wire not selected");
+		}
+		
+		if(noOfWire==5 || colourOfWire=="Green"){
+			
+			System.out.println("desired wire is selected");
+		}else{
+			System.out.println("wire not selected");
+		}
+			
+	}
+}
