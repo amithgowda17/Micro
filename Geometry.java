c387fb8edbd6f6eeb726d0d8ab0755053a319bb9
unknown <amith.s.xworkz@gmail.com>
2024-04-16 08:24:55 +0530

    Not operator

---
+++ Geometry.java
@@ -0,0 +1,28 @@
+class Geometry{
+	
+	public static void main(String []args){
+		 
+		 int geometryId=15;
+		 String geometryType="Circle";
+		 
+		 if(geometryId!=25 && geometryType=="Circle"){
+		 
+		 System.out.println("the geometry type is=="+geometryType);
+		 
+		 }else{
+			 
+			System.out.println("invalid");
+		}
+		
+		if(geometryId==15 && geometryType!="Square"){
+		 
+		 System.out.println("geometry type is verified successfully");
+		 
+		 }else{
+			 
+			System.out.println("Invalid entry");
+		}
+			 
+    }
+
+}
