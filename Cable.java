8ef9b9900708f4e0bd6348ed7e5a141b2a25c312
unknown <amith.s.xworkz@gmail.com>
2024-04-16 08:20:51 +0530

    Not operator

---
+++ Cable.java
@@ -0,0 +1,28 @@
+class Cable{
+	
+	public static void main(String []args){
+		 
+		 int cableId=45;
+		 String cableColour="Black";
+		 
+		 if(cableId==45 && cableColour!="White"){
+		 
+		 System.out.println("cable is verified successfully");
+		 
+		 }else{
+			 
+			System.out.println("Invalid entry");
+		}
+		
+		if(cableId!=45 && cableColour=="Black"){
+		 
+		 System.out.println("cable is verified successfully");
+		 
+		 }else{
+			 
+			System.out.println("Invalid entry");
+		}
+			 
+    }
+
+}
