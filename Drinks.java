7ebef81b1c45783ccc7eab479a1d72b127877701
Amith S <166138443+amithxworkz@users.noreply.github.com>
2024-04-16 19:00:34 +0530

    Not and Or operator

--- Drinks.java
+++ Drinks.java
@@ -5,20 +5,20 @@ class Drinks{
 		 int noOFBottles=10;
 		 String drinkType="Juice";
 		 
-		 if(noOFBottles==11 || drinkType=="Coco Cola"){
+		if(noOFBottles==11 || drinkType=="Coco Cola"){
 		 
 		 System.out.println("invalid");
 		 
 		}else{
 			 
 			System.out.println("the valid drink type is=="+drinkType);
-		}
+	        }
 		
 		if(noOFBottles!=13 || drinkType=="Juice"){
 		 
 		 System.out.println("drink type is successfully verified");
 		 
-		 }else{
+		}else{
 			 
 			System.out.println("Invalid entry");
 		}
