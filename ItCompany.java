12247e1cdf600b85ded9720d9a3f1e14a0691bf3
Amith S <166138443+amithxworkz@users.noreply.github.com>
2024-04-16 09:43:33 +0530

    Foor loop

--- ItCompany.java
+++ ItCompany.java
@@ -2,7 +2,7 @@ class ITCompany{
 	    
 	public static void main(String args[]){
 	
-     	int[] noOfITCompanies={21,2,3,4,5,5,44,32,112,134};
+     	        int[] noOfITCompanies={21,2,3,4,5,5,44,32,112,134};
 		byte[] emplyeesOfITCompany={112,14,12,50,47,15,99,74};
 		short[] locationsOfITCompany={1,10,3,4,4,5,8,2};
 		long[] dataOfITcompanies={1112,323,4,435,56566,8764,2324,1435435,978645};
