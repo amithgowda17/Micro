8c889bc753e5fa299d26de67e4c60c2d4a742406
unknown <amith.s.xworkz@gmail.com>
2024-04-16 08:27:46 +0530

    Not operator and Or operator

---
+++ Cups.java
@@ -0,0 +1,28 @@
+class Cups{
+	
+	public static void main(String []args){
+		 
+		 int noOfCups=3;
+		 String teamCaptain="MS Dhoni";
+		 
+		 if(noOfCups==3 || teamCaptain=="MS Dhoni"){
+		 
+		 System.out.println("the number trophy won by dhoni is=="+noOfCups);
+		 
+		 }else{
+			 
+			System.out.println("invalid");
+		}
+		
+		if(noOfCups==3 || teamCaptain=="Virat Kohli"){
+		 
+		 System.out.println("Dhoni won 3 icc tournament cup");
+		 
+		 }else{
+			 
+			System.out.println("Invalid entry");
+		}
+			 
+    }
+
+}
