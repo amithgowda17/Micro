47092e89f804df54bad901b59d9b11fc7dea685b
Amith S <166138443+amithxworkz@users.noreply.github.com>
2024-04-18 23:40:03 +0530

    Method calling

--- TapRunner.java
+++ TapRunner.java
@@ -1,7 +1,6 @@
 class TapRunner{
 	
 	public static void main(String []args){
-		
 		System.out.println("number of taps is=="+Tap.noOfTaps);
 		Tap.totalWaterTaps();
 		System.out.println("company name of the tap is=="+Tap.tapCompanyName);
