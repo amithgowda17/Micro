6e292bc3148d5b293b6a1021bb4e4bdb7ff7eb48
Amith S <166138443+amithxworkz@users.noreply.github.com>
2024-04-20 18:57:55 +0530

    For loop with break

--- Key.java
+++ Key.java
@@ -7,21 +7,18 @@ class Key{
 		char[] seriesName={'A','S','T','B','S','T','A','S','T','B'};
 		
 		for(int i=5;i<=sequenceOfKey.length;i++){
-		    
-			System.out.println("sequence of the key is=="+sequenceOfKey[i]);
+		   System.out.println("sequence of the key is=="+sequenceOfKey[i]);
 			break;
 		}
 	    
 		for(capacity=3;capacity<=10;capacity++){
-			
-			System.out.println("storage capacity is==="+capacity);
+	           System.out.println("storage capacity is==="+capacity);
 			break;
 		}
 		
 		for(int i=5;i<=seriesName.length;i++){
-		  
 		  System.out.println("series of the key is==="+seriesName[i]);
-		  break;
+		        break;
 		  
 		}
 	
