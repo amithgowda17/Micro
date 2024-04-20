a4526394a68e3e4f7a9629bed5a1f95a65ea805b
unknown <amith.s.xworkz@gmail.com>
2024-04-20 18:42:38 +0530

    For loop with break

---
+++ Key.java
@@ -0,0 +1,30 @@
+class Key{
+	
+	public static void main(String []args){
+		
+		int[] sequenceOfKey={1,2,3,4,5,6,7,8,9,10};
+		byte capacity=10;
+		char[] seriesName={'A','S','T','B','S','T','A','S','T','B'};
+		
+		for(int i=5;i<=sequenceOfKey.length;i++){
+		    
+			System.out.println("sequence of the key is=="+sequenceOfKey[i]);
+			break;
+		}
+	    
+		for(capacity=3;capacity<=10;capacity++){
+			
+			System.out.println("storage capacity is==="+capacity);
+			break;
+		}
+		
+		for(int i=5;i<=seriesName.length;i++){
+		  
+		  System.out.println("series of the key is==="+seriesName[i]);
+		  break;
+		  
+		}
+	
+	}
+	
+}
---
+++ Machine.java
@@ -0,0 +1,24 @@
+class Machine{
+	
+	public static void main(String []args){
+		
+		int[] noOfMachine={1,2,3,4,5,6,7,8};
+		char[] machineSeries={'A','C','X','Z','S','L','B','A'};
+		
+		for(int i=5;i<=noOfMachine.length;i++){
+		    
+	        System.out.println("number of bullet is=="+noOfMachine[i]);
+			break;
+		}
+	    
+	    for(int i=3;i<=machineSeries.length;i++){
+		  
+		    System.out.println("series of the key is==="+machineSeries[i]);
+		    break;
+		  
+		  
+		}
+	
+	}
+	
+}
