cbc3a87e7ceaff70a441e76304fddb6055435386
unknown <amith.s.xworkz@gmail.com>
2024-04-20 18:24:46 +0530

    For loop with break

---
+++ Bullet.java
@@ -0,0 +1,30 @@
+class Bullet{
+	
+	public static void main(String []args){
+		
+		int[] noOfBullets={1,2,3,4,5,6,7};
+		byte capacity=7;
+		short bulletCode=5674;
+		
+		for(int i=0;i<=noOfBullets.length;i++){
+		    
+			System.out.println("number of bullet is=="+noOfBullets[i]);
+			break;
+		}
+	    
+		for(capacity=1;capacity<=7;capacity++){
+			
+			System.out.println("storage capacity is==="+capacity);
+			break;
+		}
+		
+		for(bulletCode=5674;bulletCode<=5674;bulletCode++){
+		  
+		  System.out.println("bullet code is==="+bulletCode);
+		  break;
+		  
+		}
+	
+	}
+	
+}
