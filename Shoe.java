256e2ae8cb686066eab2e8f9b37ab60ce1abab23
unknown <amith.s.xworkz@gmail.com>
2024-04-21 09:25:18 +0530

    Method overloading

---
+++ Shoe.java
@@ -0,0 +1,14 @@
+class Shoe{
+	
+	public static void shoeDetails(int noOfShoes,String ownerName){
+		
+		System.out.println("total number of shoes is==="+noOfShoes);
+		System.out.println("shoe owner name is=="+ownerName);
+	}
+	
+	public static void shoeDetails(float costOfShoe){
+		
+		System.out.println("cost of the shoe is==="+costOfShoe);
+		
+	}
+}
