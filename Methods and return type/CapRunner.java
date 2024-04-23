11cabc1f28cae666c7f59f5dce8fdc772a4f70e5
Amith S <166138443+amithxworkz@users.noreply.github.com>
2024-04-23 10:55:46 +0530

    Methods and return type

--- Methods and return type/CapRunner.java
+++ Methods and return type/CapRunner.java
@@ -3,9 +3,7 @@ class CapRunner{
 	public static void main(String []args){
 		
 		Cap.capTotalVaries();
-	
-		int [] capTotal=Cap.capTotalVaries();
-		
+	        int [] capTotal=Cap.capTotalVaries();
 		for(int cap:capTotal){
 			
 			System.out.println("total numbers of caps are=="+cap);
@@ -13,9 +11,7 @@ class CapRunner{
 		}
 		
 		Cap.ownerDetails();
-		
 		String [] owner=Cap.ownerDetails();
-		
 		for(int i=0;i<owner.length;i++){
 			
 			System.out.println("cap owners name is==="+owner[i]);
