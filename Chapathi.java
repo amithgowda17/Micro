6cbbbbde55eebf55bd7fa4675eb955ffc3309a23
unknown <amith.s.xworkz@gmail.com>
2024-05-02 10:42:17 +0530

    If else condition with return type

---
+++ Chapathi.java
@@ -0,0 +1,16 @@
+class Chapathi{
+	
+	static int noOfChapathi=15;
+	
+	public static int total(){
+		
+		if(noOfChapathi==15){
+			
+			System.out.println("Many chapathis are there");
+		}else{
+			
+			System.out.println("Chapathis are not available");
+		}
+		return 10;
+	}
+}
---
+++ ChapathiRunner.java
@@ -0,0 +1,7 @@
+class ChapathiRunner{
+	
+	public static void main(String []args){
+		
+		System.out.println("total number of chapathis are==="+Chapathi.total());
+	}
+}
