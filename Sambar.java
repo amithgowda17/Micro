acf39f9bcbd17ece7f9956649f72fbe883e91439
unknown <amith.s.xworkz@gmail.com>
2024-05-02 10:28:08 +0530

    If else condition with return type

---
+++ Sambar.java
@@ -0,0 +1,33 @@
+class Sambar{
+	
+    public static int noOfSamabars=10;
+	public static String sambarName="Mutton Sambar";
+	
+    public static int total(){
+		
+		if (noOfSamabars==10){
+			
+			System.out.println("there are many sambars");
+			return noOfSamabars;
+
+		}else{
+			
+			System.out.println("no sambars available");
+			return noOfSamabars;
+			
+		}
+	}
+	
+	public static String name(){
+		
+		if (sambarName=="Bele Sambar"){
+			
+			System.out.println("Sambar doesn't matched");
+
+		}else{
+			
+			System.out.println("sambar name matched as");
+		}
+		return sambarName;
+	}
+}
---
+++ SambarRunner.java
@@ -0,0 +1,8 @@
+class SambarRunner{
+	
+	public static void main(String []args){
+		System.out.println("total number of sambar are=="+Sambar.total());
+		System.out.println("name of the sambar is==="+Sambar.name());
+		
+	}
+}
