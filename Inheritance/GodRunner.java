0db714aa509e45626a987c69f680baf07528b475
unknown <amith.s.xworkz@gmail.com>
2024-05-22 22:19:00 +0530

    Inheritance with super keyword

---
+++ Inheritance/GodRunner.java
@@ -0,0 +1,10 @@
+class GodRunner{
+	
+	public static void main(String []args){
+		
+		
+		GodSon god=new GodSon();
+		god.nameSon("Ganesha");
+		god.nameFather();
+	}
+}
---
+++ Inheritance/GodSon.java
@@ -0,0 +1,12 @@
+class GodSon extends GodFather{
+	
+	String godName;
+	
+	public void nameSon(String godName){
+		
+		System.out.println("the name of the god is==="+this.godName);
+		this.godName=godName;
+		System.out.println("the name of the god is==="+this.godName);
+		super.godName=godName;
+	}
+}
