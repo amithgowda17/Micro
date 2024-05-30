7e42b735a5c23167bcb4049b788972e9ef79b3bc
unknown <amith.s.xworkz@gmail.com>
2024-05-30 10:11:10 +0530

    Down Typecasting

---
+++ Inheritance/Fan.java
@@ -0,0 +1,26 @@
+class Fan{
+	
+	public static void main(String [] args){
+		
+		int price=100;
+		double gst = price;
+		System.out.println("this is implicit cost="+gst);
+		
+		double cgst=1000.123;
+		int rate = (int)cgst;
+		System.out.println("this is expilict cost="+rate);
+		
+		int data=65;
+        char name=(char)data;
+        System.out.println("this is convertion of int to char=="+name);	 
+	 
+        char nameLetter='c';
+	    int valueOfTheLetter=nameLetter;
+	    System.out.println("this is convertion of char to int==="+valueOfTheLetter);	 
+	 
+	    boolean ifFan=true;
+	    int fanValue = ifFan ?1:0 ;
+	    System.out.println("this is convertion of boolean  to int==="+fanValue);	 
+	 
+	}
+}
