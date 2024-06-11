250638495f84052f61bb7433325f789015371915
unknown <amith.s.xworkz@gmail.com>
2024-06-11 16:46:47 +0530

    interface

---
+++ Abstraction/abstraction/src/com/xworkz/multipleinheritance/things/TeslaEngine.java
@@ -0,0 +1,12 @@
+package com.xworkz.multipleinheritance.things;
+
+public interface TeslaEngine {
+	
+	public void capacity();
+	public void cc();
+	public void cost();
+	public void companyNameOfEngine();
+	public void quality();
+
+
+}
---
+++ Abstraction/abstraction/src/com/xworkz/multipleinheritance/things/TeslaSeats.java
@@ -0,0 +1,11 @@
+package com.xworkz.multipleinheritance.things;
+
+public interface TeslaSeats {
+	
+	public void totalSeats();
+	public void brand();
+	public void costOfSeats();
+	public void ifSeatsAvailable();
+	public void qualityOfSeats();
+
+}
