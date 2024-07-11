07395f70fb80c64a8bcc1657c5b8ccb519996e46
unknown <amith.s.xworkz@gmail.com>
2024-07-11 16:59:56 +0530

    comparator and comparable

--- collection/collection/src/com/xworkz/collection/dto/BikeCollectionDto.java
+++ collection/collection/src/com/xworkz/collection/dto/BikeCollectionDto.java
@@ -1,6 +1,6 @@
 package com.xworkz.collection.dto;
 
-public class BikeCollectionDto {
+public class BikeCollectionDto implements Comparable<BikeCollectionDto>{
 	
 	private int price;
 	private String companyName;
@@ -50,6 +50,51 @@ public class BikeCollectionDto {
 	@Override
 	public String toString() {
 		return "Bike [price=" + price + ", companyName=" + companyName + ", address=" + address + ", cc=" + cc + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + cc;
+		result = prime * result + ((companyName == null) ? 0 : companyName.hashCode());
+		result = prime * result + price;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BikeCollectionDto other = (BikeCollectionDto) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (cc != other.cc)
+			return false;
+		if (companyName == null) {
+			if (other.companyName != null)
+				return false;
+		} else if (!companyName.equals(other.companyName))
+			return false;
+		if (price != other.price)
+			return false;
+		return true;
+	}
+
+	@Override
+	public int compareTo(BikeCollectionDto o) {
+		if(this.getCc()>o.getCc()) {
+			return 1;
+		}
+		return -1;
 	}
 	
 	
