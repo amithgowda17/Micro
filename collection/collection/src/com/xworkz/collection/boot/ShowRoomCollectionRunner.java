21521dbe24743687a6963e0df90d9e058cb6a7a1
unknown <amith.s.xworkz@gmail.com>
2024-07-30 08:42:59 +0530

    collection

--- collection/collection/src/com/xworkz/collection/boot/ShowRoomCollectionRunner.java
+++ collection/collection/src/com/xworkz/collection/boot/ShowRoomCollectionRunner.java
@@ -10,13 +10,12 @@ import com.xworkz.collection.dto.ShowRoomCollectionDto;
 public class ShowRoomCollectionRunner {
 
 	public static void main(String[] args) {
-		
+
 		ShowRoomCollectionDto dto = new ShowRoomCollectionDto(300000, "Skoda", "Hebbal");
 		ShowRoomCollectionDto dto1 = new ShowRoomCollectionDto(5000000, "Audi", "RRNagar");
 		ShowRoomCollectionDto dto2 = new ShowRoomCollectionDto(1000000, "Hyundai", "BtmLayout");
 		ShowRoomCollectionDto dto3 = new ShowRoomCollectionDto(2000000, "BMW", "Tolgate");
-		
-		
+
 		Comparator<ShowRoomCollectionDto> comparator = new Comparator<ShowRoomCollectionDto>() {
 
 			@Override
@@ -27,23 +26,19 @@ public class ShowRoomCollectionRunner {
 				return -1;
 			}
 		};
-		
-		
+
 		List<ShowRoomCollectionDto> list = new ArrayList<ShowRoomCollectionDto>();
-		
-		
+
 		list.add(dto);
 		list.add(dto2);
 		list.add(dto3);
 		list.add(dto1);
-		
-		
-		
-		Collections.sort(list,comparator);
-		
+
+		Collections.sort(list, comparator);
+
 		for (ShowRoomCollectionDto showRoomCollection : list) {
 			System.out.println(showRoomCollection);
 		}
-		
+
 	}
 }
